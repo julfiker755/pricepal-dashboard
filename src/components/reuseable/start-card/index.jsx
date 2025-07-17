8a1b82a396a3032e6f44ad8ef6d0748550ef9891
julfiker755 <julfiker755.bd@gmail.com>
2025-07-17 09:35:15 +0600

    dashboard overview update

--- src/components/reuseable/start-card/index.jsx
+++ src/components/reuseable/start-card/index.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 export default function StartCard({ title, value, Icon }) {
     return (
-        <div className="flex justify-between items-center rounded-2xl border border-[#00000033] bg-white px-6 pb-8 shadow-sm">
-            <div className='mt-14'>
-                <p className="text-3xl text-gray-800">{title}</p>
+        <div className="flex justify-between items-center rounded-2xl border border-[#00000033] bg-white px-6  py-10 shadow-sm">
+            <div>
+                <p className="text-2xl text-gray-800">{title}</p>
                 <h2 className="text-4xl font-semibold text-black mt-1">{value}</h2>
             </div>
             <div className="rounded-full bg-[#F3FFDD] p-4">
