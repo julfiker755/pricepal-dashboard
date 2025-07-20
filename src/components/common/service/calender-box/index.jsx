a6999ce97c9234fd7547e0afbd58d51c9a882a50
julfiker755 <julfiker755.bd@gmail.com>
2025-07-20 18:57:18 +0600

    service added

--- src/components/common/service/calender-box/index.jsx
+++ src/components/common/service/calender-box/index.jsx
@@ -7,12 +7,13 @@ export function Calenderbox() {
   const [date, setDate] = React.useState(new Date())
 
   return (
-    <Calendar
-      mode="single"
-      selected={date}
-      onSelect={setDate}
-      className="h-[350px] w-[400px] rounded-md border shadow-sm"
-      captionLayout="dropdown"
-    />
+    // <Calendar
+    //   mode="single"
+    //   selected={date}
+    //   onSelect={setDate}
+    //   className="h-[350px] w-[400px] rounded-md border shadow-sm"
+    //   captionLayout="dropdown"
+    // />
+    <h1>Date page</h1>
   )
 }
